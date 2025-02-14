44dd79d29a0ddcfd0c80f138878885075998b26c
Melashik <melashik1@gmail.com>
2025-02-14 23:35:43 +0200

    Update homework 5.py

--- homework 5.py
+++ homework 5.py
@@ -1,1 +1,9 @@
+import inspect
+import tkinter
+
+print(inspect.ismodule(tkinter))
+print(inspect.isclass(tkinter))
+print(inspect.isfunction(tkinter))
+print(inspect.getmodule(tkinter.Tk))
+print(inspect.getmodule(list)) 
 
